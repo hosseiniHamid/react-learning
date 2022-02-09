2e1d39bb52c03d36b91c6b52898699a2040806a1
hosseinihamid <syd.hamidreza.hosseinii1@gmail.com>
2022-02-09 04:26:50 +0330

    add home page

--- src/App.js
+++ src/App.js
@@ -1,93 +1,29 @@
-import React from "react";
-import './App.css';
+import FirstProject from "./firstProject";
+import { BrowserRouter, Route ,Link ,Routes} from "react-router-dom";
 
-const notifications = [
-    {
-        time: "12:00",
-        description: "Loan Them Sprint check"
-    }
-];
 
-class App extends React.Component {
-
-    numbers = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-
-    days() {
-        const calendar_array = [];
-        let flag = true;
-        for (let day = 30; day !== 13 || flag; day++) {
-            calendar_array.push(day);
-            if (day === 31 || day === 28) {
-                flag =! (day === 28);
-                day = 0;
-            }
-        }
-        return calendar_array;
-    }
-
-    renderNotification(item, index) {
-        return (
-            <div className="notification-item" key={index}>
-                <span className="notification--icon">
-                    icon
-                </span>
-                <div className="notification__description-box">
-                    <span className="notification__description-box--time">{item.time}</span>
-                    <span
-                        className="notification__description-box--description">{item.description}</span>
-                </div>
-            </div>)
-    }
-
-    render() {
-        return (
-            <div className="div">
-                <div className="container">
-                    <div className="left-side">
-                        <div className="notification-wrapper">
-                            {notifications.map(this.renderNotification)}
-                        </div>
-                        <div className="footer">
-                            <div className="footer__setting">
-                                <label htmlFor="disturb" className="footer__setting--text">Do Not Disturb</label>
-                                <input type="checkbox" id="disturb" name="disturb"/>
-                            </div>
-                            <input className="footer--btn" value="Clear" type="button" disabled={true}/>
-                        </div>
-                    </div>
-                    <div className="right-side">
-                        <h1 className="right-side--header">February 3 2022</h1>
-                        <div className="calendar">
-                            <div className="calendar__header">
-                                <div className="calendar__header__month">
-                                    <span className="calendar__header__month--left-arrow"><b>{'<'}</b></span>
-                                    <label className="calendar__header__month--title"><b>{'February'}</b></label>
-                                    <span className="calendar__header__month--right-arrow"><b>{'>'}</b></span>
-                                </div>
-                                <ul className="calendar--header__week-day">
-                                    {this.numbers.map((number) => {
-                                        return (
-                                            <li className="calendar__header__week-day--day">{number}</li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                            <ul className="calendar__days">
-                                {this.days().map((item) => (
-                                    <li className={`calendar__days__day ${ item.disable ? "disabled" : "" }`}>
-                                        {item.label}
-                                        {item.event ?
-                                            <span className="calendar__days__day--event"/> : item.event = false}
-                                    </li>
-                                ))}
-                                <span className="calendar__days__day--event"/>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" exact={false} element={<Home/>}/>
+                    <Route path="/first-project" element={<FirstProject/>}/>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    )
 }
 
 export default App;
+
+function Home() {
+    return (
+        <div>
+            <h1>This is the home page</h1>
+            <Link to="/first-project">Click to view My first project</Link>
+        </div>
+    );
+}
+
+
